8c03eae85759c27d0ea0ed1fc7a9977543fa93df
ayrtondutchman <ayrtonanemaet@gmail.com>
2021-06-27 20:53:20 +0200

    tests

--- tests/clvm/test_chialisp_deserialization.py
+++ tests/clvm/test_chialisp_deserialization.py
@@ -4,7 +4,7 @@ from hddoge.types.blockchain_format.program import Program, INFINITE_COST
 from hddoge.util.byte_types import hexstr_to_bytes
 from hddoge.wallet.puzzles.load_clvm import load_clvm
 
-DESERIALIZE_MOD = load_clvm("chialisp_deserialisation.clvm", package_or_requirement="hddoge.wallet.puzzles")
+DESERIALIZE_MOD = load_clvm("hddogelisp_deserialisation.clvm", package_or_requirement="hddoge.wallet.puzzles")
 
 
 def serialized_atom_overflow(size):
